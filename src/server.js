f7b1f3137c550baa7f214144e5c17421bf602d24
Leandro Gomes <lgds2809@gmail.com>
2023-08-14 17:30:00 -0300

    routers

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
-const express = require('express')
+//const express = require('express')  
+import express from 'express'
+import userRouter from './routes/userRouter.js'
 
 const api = express()
 
@@ -6,6 +8,8 @@ api.get('/', (req, res) => {
     res.json({message: "Bem-vindo a nossa API"})
 })
 
+api.use('/user', userRouter)
+
 api.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000. http://localhost:3000')
+    console.log('Servidor rodando na porta 3000! http://localhost:3000')
 })
